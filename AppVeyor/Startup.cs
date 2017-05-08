aff5c035c37b0e7e39f0924a5e0ab0fe817392f0
Nam Nguyen Thanh <namnt3108@gmail.com>
2017-05-08 22:38:34 +0700

    Added project

---
+++ AppVeyor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppVeyor.Startup))]
+namespace AppVeyor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
